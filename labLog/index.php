b49b93f3ef3d570849fecef4ca2492ac78bbc029
Tomáš Apeltauer <apelttom@gmail.com>
2018-05-31 12:50:45 +0800

    NEW: facelift for my personal page
    * php index and new graphics

---
+++ labLog/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+	<title>Tomáš Apeltauer</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Tomáš Apeltauer, apelttom@gmail.com">
+	<meta name="keywords" content="PhD,research,Apeltauer,Tomáš">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="css/styles.css" media="screen" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="w3-light-grey">
+
+	<!-- Icon Bar (Sidebar - hidden on small screens) -->
+	<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+		<!-- Avatar image in top left corner -->
+		<a href="#"><img src="img/template/avatar_smoke.png" style="width:100%"></a>
+		<a href="#" class="w3-light-grey, w3-bar-item, w3-button, w3-padding-large">
+			<i class="fa fa-home w3-xxlarge"></i>
+			<p>HOME</p>
+		</a>
+		<a href="s_taliro.html" n:class="w3-hover-light-grey, w3-bar-item, w3-button, w3-padding-large">
+			<i class="fa fa-university w3-xxlarge"></i>
+			<p>RESEARCH</p>
+		</a>
+	</nav>
+
+	<!-- Navbar on small screens (Hidden on medium and large screens) -->
+	<div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+	  <div class="w3-bar w3-grey w3-opacity w3-hover-opacity-off w3-center w3-small">
+	    <a n:href="#" n:class="w3-dark-grey, w3-bar-item, w3-button" style="width:50% !important">HOME</a>
+	    <a n:href="s_taliro.html"  n:class="w3-bar-item, w3-button" style="width:50% !important">RESEARCH</a>
+	  </div>
+	</div>
+
+
+	<!-- Page Content -->
+	<div class="w3-padding-large" id="main">
+    <!-- Header/Home -->
+      <header class="w3-container w3-padding-32 w3-center w3-light-grey" id="home">
+     	 <h1 class="w3-jumbo"><span class="w3-hide-small">I'm</span> Tomáš Apeltauer<span class="w3-hide-small">.</span></h1>
+     	 <p>PhD student</p>
+     	 <img src="img/template/tomas.jpg" alt="man" class="w3-image img-circle" width="992" height="1108">
+      </header>
+
+      <!-- About Section -->
+      <div class="w3-content w3-justify w3-text-black w3-padding-64" id="about">
+      <h2 class="w3-text-black">Tomáš Apeltauer</h2>
+      <hr style="width:200px" class="w3-opacity">
+      <p>
+        I am a second year PhD student at Czech Technical University in Prague, Faculty of Information Technology, Department of the Digital Design. The topic of my reasearch is <i>"Automated testing of models of cyber-physical systems"</i>.
+      </p>
+
+      <!-- Contact Section -->
+      <div class="w3-padding-64 w3-content w3-text-grey" id="contact">
+     	 <h2 class="w3-text-black">Contact Me</h2>
+     	 <hr style="width:200px" class="w3-opacity">
+
+     	 <div class="w3-section">
+     		 <p><i class="fa fa-map-marker fa-fw w3-text-black w3-xxlarge w3-margin-right"></i>Room A:1051, CTU in Prague</p>
+     		 <p><i class="fa fa-phone fa-fw w3-text-black w3-xxlarge w3-margin-right"></i> Phone: +852 5932 2746</p>
+     		 <p><i class="fa fa-envelope fa-fw w3-text-black w3-xxlarge w3-margin-right"> </i> Email: <a href="mailto:apelttom@fit.cvut.cz" class="tooltip">apelttom@fit.cvut.cz<span class="tooltiptext">You can write me about anything!</span></a></p>
+     	 </div><br>
+
+      <!-- End Contact Section -->
+      </div>
+
+
+		<!-- Footer -->
+		<footer class="w3-content w3-padding-64 w3-text-black w3-xlarge">
+			<a href="https://github.com/apelttom" target="_blank"><i class="fa fa-github w3-hover-opacity"></i></a>
+			<a href="https://www.linkedin.com/in/apelttom/" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
+			<p class="w3-medium">Made by Tomáš Apeltauer © 2018. Powered by HTML5, <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a>, PHP5 and <a href="https://nette.org/cs/" target="_blank">Nette</a></p>
+		<!-- End footer -->
+		</footer>
+
+	<!-- END PAGE CONTENT -->
+	</div>
+</body>
+</html>
